de7adbe7dd1cd86358324077cb5b502216d4000f
Nikhil Junghare <nikhilj@prorigo.com>
2026-07-10 10:18:22 +0530

    understanding first branch

---
+++ Third.js
@@ -0,0 +1,6 @@
+let num1 = Number(prompt("Enter first number:"));
+let num2 = Number(prompt("Enter second number:"));
+
+let sum = num1 + num2;
+
+alert("Sum = " + sum);
--- first .js
+++ first .js
@@ -4,3 +4,13 @@ console.log("Hello " + name);
 let name = "Nikhil";
 console.log("Hello " + name);
 
+console.log("New Change");
+
+// Add two numbers in JavaScript
+
+let num1 = 10;
+let num2 = 20;
+
+let sum = num1 + num2;
+
+console.log("Sum =", sum);
